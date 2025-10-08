e902df923a22c8a679b4eb4fed6f8c7dcd165ce9
ElizabethGreeff <ginaoak2000@gmail.com>
2025-10-08 17:58:22 +0200

    menu complete

---
+++ project/scripts/index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("show");
+    });
+
+    function showNextSlide() {
+    if (slides.length === 0) return; // Skip if no slideshow (desktop)
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+    }
+
+    setInterval(showNextSlide, 4000);
+});
